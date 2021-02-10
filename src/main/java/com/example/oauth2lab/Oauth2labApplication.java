6ed0a2b8b88276d6d47056f694f8a67915f5702a
shantel-dot <taylorteep2@yahoo.com>
2021-02-10 06:09:15 -0600

    Initial commit 1st commit

---
+++ src/main/java/com/example/oauth2lab/Oauth2labApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauth2lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2labApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2labApplication.class, args);
+	}
+
+}
